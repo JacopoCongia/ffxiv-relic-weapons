a3fd42b81b60256a5a7fef704ca5a2baa8fc2dfb
jacopo.congia <jacopo.congia@gmail.com>
2023-04-17 09:27:48 +0200

    Jacopocongia/index-page-links
    * Local Storage Implementation
    
    * Updated Index Page Links

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,43 @@
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <>
-      <div className="bold bold text-bold flex flex-col text-center font-bold text-white">
-        <div className="text-5 text-10 bg-emerald-900 p-10 text-3xl">
-          <h1>Final Fantasy XIV - Relic Weapons</h1>
+  const data = [
+    {
+      id: 0,
+      title: "Endwalker - Manderville Weapons",
+      url: "/Endwalker"
+    },
+    {
+      id: 1,
+      title: "Shadowbringers - Resistance Weapon",
+      url: "/Shadowbringers"
+    },
+    {
+      id: 2,
+      title: "Stormblood - Eurekan Weapons - (Coming Soon)",
+      url: "/"
+    }
+  ];
+
+  const renderedElements = data.map((element) => {
+    return (
+      <Link
+        key={element.id}
+        href={element.url}
+      >
+        <div className="text-5 text-10 cursor-pointer bg-teal-700 p-7 text-2xl hover:opacity-90">
+          <h2>{element.title}</h2>
         </div>
-        <Link href="/Endwalker">
-          <div className="text-5 text-10 cursor-pointer bg-teal-700 p-7 text-2xl hover:opacity-90">
-            <h2>Endwalker - Manderville Weapons</h2>
-          </div>
-        </Link>
-        <Link href="/Shadowbringers">
-          <div>
-            <h2 className="text-5 text-10 cursor-pointer bg-teal-700 p-7 text-2xl hover:opacity-90">
-              Shadowbringers - Resistance Weapons
-            </h2>
-          </div>
-        </Link>
+      </Link>
+    );
+  });
+
+  return (
+    <div className="bold bold text-bold flex flex-col text-center font-bold text-white">
+      <div className="text-5 text-10 bg-emerald-900 p-10 text-3xl">
+        <h1>Final Fantasy XIV - Relic Weapons</h1>
       </div>
-    </>
+      {renderedElements}
+    </div>
   );
 }
